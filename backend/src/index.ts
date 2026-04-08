d42c40a696f6409a0b1998ec6ec07e1ec504fca1
mariela chamo <mchamopaco@gmail.com>
2026-04-07 21:30:51 -0400

    fix cambios

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,7 +31,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001','https://tu-frontend-en-vercel.vercel.app'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
